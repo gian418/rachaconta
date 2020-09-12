09ae30aa38147d089ab2058d23ae918fe48dba50
Giancarlo Haack Albano <gian418@gmail.com>
2020-09-12 11:06:33 -0300

    Adicionando Swagger e criando o arquivo do TravisCI

--- src/main/java/br/com/gianhaack/rachaconta/resources/RachaContaResource.java
+++ src/main/java/br/com/gianhaack/rachaconta/resources/RachaContaResource.java
@@ -6,6 +6,10 @@ import static org.springframework.http.ResponseEntity.ok;
 import br.com.gianhaack.rachaconta.dtos.ContaDTO;
 import br.com.gianhaack.rachaconta.dtos.ResponseContaDTO;
 import br.com.gianhaack.rachaconta.services.RachaContaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -22,6 +26,12 @@ public class RachaContaResource {
     @Autowired
     RachaContaService rachaContaService;
 
+    @ApiOperation("Retorna o resultado do calcudo de quanto cada pessoa deve pagar")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna o resultado do calculo"),
+            @ApiResponse(code = 400, message = "Problema algum atributo passado"),
+            @ApiResponse(code = 500, message = "Ocorreu algum erro nos servidor"),
+    })
     @RequestMapping(value = "/rachar", method = POST)
     public ResponseEntity racharConta(@RequestBody @Valid ContaDTO contaDTO) {
         ResponseContaDTO responseContaDTO = rachaContaService.calcular(contaDTO);
